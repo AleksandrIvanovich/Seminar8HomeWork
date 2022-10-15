7014c4a9a4e59f33b5663fafcf851793fe09e8a4
Aleksandr <fomichov73@mail.ru>
2022-10-15 14:29:15 +0300

    Added solution task 58

--- HomeWork8/Task58/Program.cs
+++ HomeWork8/Task58/Program.cs
@@ -1,54 +1,69 @@
-﻿/*Задача 56: Задайте прямоугольный двумерный массив. 
-Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-*/
+﻿/*Задача 58: Задайте две матрицы. Напишите программу,
+ которая будет находить произведение двух матриц.
+ */
 int GetNumber(string message)
 {
-    Console.WriteLine(message);
-    int number = int.Parse(Console.ReadLine() ?? "");
-    return number;
+  Console.WriteLine(message);
+  int number = int.Parse(Console.ReadLine() ?? "");
+  return number;
 }
-int m = GetNumber("Введите число (кличество срок)");
-int n = GetNumber("Введите число (кличество столбцов)");
 
-Console.WriteLine("Матрица:");
-int[,] matrix = new int[m, n];
-Random rnd = new Random();
-for (int i = 0; i < matrix.GetLength(0); i++)
+int [,] InitMatrix(int rov, int col)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
+  int[,] matrix = new int[rov,col];
+  Random rnd = new Random();
+ 
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+     for (int j = 0; j < matrix.GetLength(1); j++)
+     {
         matrix[i, j] = rnd.Next(1, 10);
-        Console.Write(matrix[i, j] + " ");
-    }
-    Console.WriteLine();
+     }
+  }
+  return matrix;
 }
-void SumStringMatrix(int[,] matrix)
+void PrintMatrix(int[,] matrix)
 {
-    int index = 0;
-    int minsum = 0;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        int sum = 0;
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            sum += matrix[i, j];
+            Console.Write($"{matrix[i, j]} ");
         }
-        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
-        if (i == 0)
-        {
-            minsum = sum;
-        }
-        else if (sum < minsum)
-        {
-            minsum = sum;
-            index = i;
-        }
+        Console.WriteLine();
     }
-    string line = string.Empty;
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        line += matrix[index, j] + " ";
-    }
-    Console.WriteLine($"Строка {index + 1}, с минимальной суммой элементов равной: {minsum}. ");
 }
-SumStringMatrix(matrix);
+
+int[,] DivMatrix(int[,] matrix1, int[,] matrix2)
+{
+    var matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    if (matrix1.GetLength(1) == matrix2.GetLength(0))
+    {
+        for (int i = 0; i < matrix3.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix3.GetLength(1); j++)
+            {
+                matrix3[i, j] = 0;
+                for (int n = 0; n < matrix1.GetLength(1); n++)
+                {
+                    matrix3[i, j] += matrix1[i, n] * matrix2[n, j];
+                }
+            }
+        }
+    }
+    return matrix3;
+}
+
+int n = GetNumber("Введите число (кличество строк первой и столбцов второй матриц)");
+int m = GetNumber("Введите число (кличество столбцов первой матрицы:)");
+int a = GetNumber("Введите число (кличество строк второй матрицы:)");
+
+int[,] matrix1 = InitMatrix(m, n);
+int[,] matrix2 = InitMatrix(n, a);
+Console.WriteLine("Первая матрица:");
+PrintMatrix(matrix1);
+Console.WriteLine("Вторая матрица:");
+PrintMatrix(matrix2);
+Console.WriteLine("Произведение первой и второй матриц:");
+PrintMatrix(DivMatrix(matrix1, matrix2));
+    
