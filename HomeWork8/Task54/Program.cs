8be50960a2bdd87a396423b2ff352503a7c236e9
Aleksandr <fomichov73@mail.ru>
2022-10-15 10:12:58 +0300

    Added solution task 54

---
+++ HomeWork8/Task54/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию
+// элементы каждой строки двумерного массива.
+
+int GetNumber(string message)
+{
+  Console.WriteLine(message);
+  int number = int.Parse(Console.ReadLine() ?? "");
+  return number;
+}
+int m = GetNumber("Введите число (кличество столбцов)");
+int n = GetNumber("Введите число (кличество срок)");
+
+int[,] matrix = new int[m, n];
+int[,] sortMatrix = new int[m, n]; 
+
+Console.WriteLine("Матрица:");  
+Random ran = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = ran.Next(1, 10);
+            sortMatrix[i, j] = matrix[i, j];
+            Console.Write("{0}\t", matrix[i, j]);
+        }
+    Console.WriteLine();
+    }
+
+int[] temp = new int[n];
+ 
+Console.WriteLine("\nСтроки матрицы сортированны по убыванию: ");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            temp[j] = sortMatrix[i, j];
+            Array.Sort(temp);
+            Array.Reverse(temp);
+            for (int k = 0; k < n; k++)
+            {
+                sortMatrix[i, k] = temp[k];
+                Console.Write("{0}\t", sortMatrix[i,k]);
+            }
+    Console.WriteLine();
+}
---
+++ HomeWork8/Task56/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = 4;
+            int[,] a = new int[n, n];
+            int[,] b = new int[n, n]; //массив для сортировки по строкам
+            int[,] c = new int[n, n]; //массив для сортировки по столбцам
+    Console.WriteLine("Матрица:");  
+            Random ran = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = ran.Next(1, 10);
+                    b[i, j] = a[i, j];
+                    c[i, j] = a[i, j];
+                    Console.Write("{0}\t", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+            int[] temp = new int[n];
+ 
+            Console.WriteLine("\nСтроки сортированны по убыванию: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    temp[j] = b[i, j];
+                Array.Sort(temp);
+                Array.Reverse(temp);
+                for (int k = 0; k < n; k++)
+                {
+                    b[i, k] = temp[k];
+                    Console.Write("{0}\t", b[i, k]);
+                }
+                Console.WriteLine();
+            }
