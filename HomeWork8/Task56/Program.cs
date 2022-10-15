2f9790400599140582aab10bdb839945d78e2aeb
Aleksandr <fomichov73@mail.ru>
2022-10-15 12:03:50 +0300

    Added solution task 56

--- HomeWork8/Task56/Program.cs
+++ HomeWork8/Task56/Program.cs
@@ -1,33 +1,53 @@
-﻿int n = 4;
-            int[,] a = new int[n, n];
-            int[,] b = new int[n, n]; //массив для сортировки по строкам
-            int[,] c = new int[n, n]; //массив для сортировки по столбцам
-    Console.WriteLine("Матрица:");  
-            Random ran = new Random();
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    a[i, j] = ran.Next(1, 10);
-                    b[i, j] = a[i, j];
-                    c[i, j] = a[i, j];
-                    Console.Write("{0}\t", a[i, j]);
-                }
-                Console.WriteLine();
-            }
-            int[] temp = new int[n];
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+*/
+
+int GetNumber(string message)
+{
+  Console.WriteLine(message);
+  int number = int.Parse(Console.ReadLine() ?? "");
+  return number;
+}
+int m = GetNumber("Введите число (кличество столбцов)");
+int n = GetNumber("Введите число (кличество срок)");
+
+int[,] matrix = new int[m, n];
+int[,] summMatrix = new int[m, n]; 
+
+Console.WriteLine("Матрица:");  
+Random ran = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = ran.Next(1, 10);
+            summMatrix[i, j] = matrix[i, j];
+            Console.Write("{0}\t", matrix[i, j]);
+        }
+    Console.WriteLine();
+    }
+
+int[] temp = new int[n];
  
-            Console.WriteLine("\nСтроки сортированны по убыванию: ");
-            for (int i = 0; i < n; i++)
+Console.WriteLine("\nСтроки матрицы сортированны по убыванию: ");
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            temp[j] = summMatrix[i, j];
+            int summ = 0;
+            for (int k = 0; k < matrix.GetLength(1); k++)
             {
-                for (int j = 0; j < n; j++)
-                    temp[j] = b[i, j];
-                Array.Sort(temp);
-                Array.Reverse(temp);
-                for (int k = 0; k < n; k++)
-                {
-                    b[i, k] = temp[k];
-                    Console.Write("{0}\t", b[i, k]);
-                }
-                Console.WriteLine();
+                
+                summ = summ + temp[k];
+                Console.Write("{0}\t", summ);
             }
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int maxNum = matrix[]
+            
+        }    
+
+
+    Console.WriteLine();
+}
---
+++ HomeWork8/Task58/Program.cs
@@ -0,0 +1,53 @@
+﻿int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+int m = GetNumber("Введите число (кличество срок)");
+int n = GetNumber("Введите число (кличество столбцов)");
+
+Console.WriteLine("Матрица:");
+int[,] matrix = new int[m, n];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(1, 10);
+        Console.Write("{0}\t", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
+
+void SumStringMatrix(int[,] matrix)
+{
+    int index = 0;
+    int minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        line += matrix[index, j] + " ";
+    }
+    Console.WriteLine($"Строка {index + 1}, с минимальной суммой элементов равной: {minsum}. ");
+}
+Console.WriteLine("Матрица:");
+SumStringMatrix(matrix);
